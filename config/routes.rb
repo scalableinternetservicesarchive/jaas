49578b78f5fc20966e54ca3b0c470791ab661855
Jonathan Cheung <jcheung142@gmail.com>
2016-11-03 16:43:19 -0700

    fixed layout issues with landing page and register page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_scope :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get "/sign_in", to: "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up", to: "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
   end
 
   devise_for :users
