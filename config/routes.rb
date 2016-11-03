e16073fee288751bcc65a5cf75e6d6192dd4a150
Jonathan Cheung <jcheung142@gmail.com>
2016-11-03 01:01:40 -0700

    added registration page, need to fix CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
 
   devise_for :users
   root 'static_pages#landing'
