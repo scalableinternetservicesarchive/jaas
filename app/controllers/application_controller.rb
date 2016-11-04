6a81b0d200d87ed05f2504ceb17e323cd19cfaf3
Jonathan Cheung <jcheung142@gmail.com>
2016-11-03 17:23:37 -0700

    skeleton match_make page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :school, :phone])
   end
+
+def after_sign_in_path_for(resource_or_scope)
+	make_match_path
+end
 end
