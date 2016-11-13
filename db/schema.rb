a530653b01f8293d73153414301afc0249fe57c0
Stanley Hsu <sths0614@ucla.edu>
2016-11-13 01:27:55 -0800

    add confirmable to user and set up mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110194605) do
+ActiveRecord::Schema.define(version: 20161113061036) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
@@ -77,6 +77,10 @@ ActiveRecord::Schema.define(version: 20161110194605) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
