778e5cf05ab29a7919afb52dd78bc95f6ec7f312
Aaron Cheng <ac4466@gmail.com>
2016-11-16 06:50:32 +0000

    Finish user matching algorithm

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -12,43 +12,261 @@ class MatchesController < ApplicationController
   end
 
   def match_request
-    # if no existing matches, create new one
-    new_match = Match.new
-    new_match.user1Id = current_user.id
-    new_match.status = 'pending'
-    if params[:mondayAvailable] == 'true'
-      new_match.mondayStartTime = params[:mondayTimes][0]
-      new_match.mondayEndTime = params[:mondayTimes][1]
+    # check if user already submitted a request
+    user1_matches = Match.pending_or_complete.with_user1(current_user.id.to_s).size
+    user2_matches = Match.pending_or_complete.with_user2(current_user.id.to_s).size
+    if user1_matches > 0 || user2_matches > 0
+      return head(:bad_request)
     end
-    if params[:tuesdayAvailable] == 'true'
-      new_match.tuesdayStartTime = params[:tuesdayTimes][0]
-      new_match.tuesdayEndTime = params[:tuesdayTimes][1]
-    end
-    if params[:wednesdayAvailable] == 'true'
-      new_match.wednesdayStartTime = params[:wednesdayTimes][0]
-      new_match.wednesdayEndTime = params[:wednesdayTimes][1]
-    end
-    if params[:thursdayAvailable] == 'true'
-      new_match.thursdayStartTime = params[:thursdayTimes][0]
-      new_match.thursdayEndTime = params[:thursdayTimes][1]
-    end
-    if params[:fridayAvailable] == 'true'
-      new_match.fridayStartTime = params[:fridayTimes][0]
-      new_match.fridayEndTime = params[:fridayTimes][1]
-    end
-    if params[:saturdayAvailable] == 'true'
-      new_match.saturdayStartTime = params[:saturdayTimes][0]
-      new_match.saturdayEndTime = params[:saturdayTimes][1]
-    end
-    if params[:sundayAvailable] == 'true'
-      new_match.sundayStartTime = params[:sundayTimes][0]
-      new_match.sundayEndTime = params[:sundayTimes][1]
-    end
-    new_match.save!
     
-    params[:cuisines].each do |cuisine|
-      new_match_to_food = MatchToFood.new(matchId: new_match.id, foodId: cuisine)
-      new_match_to_food.save
+    # get all pending matches, from same school, and with at least one food in common
+    matches = Match.pending.with_school(current_user.school).with_foods(params[:cuisines])
+    
+    # get a match that at least has one time in common
+    picked_match = nil
+    matches.each do |match|
+      if match.mondayStartTime != nil && params[:mondayAvailable] == 'true'
+        if match.mondayStartTime == params[:mondayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.mondayStartTime < params[:mondayTimes][0].to_i && match.mondayEndTime > params[:mondayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.mondayStartTime > params[:mondayTimes][0].to_i && match.mondayStartTime < params[:mondayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+      if match.tuesdayStartTime != nil && params[:tuesdayAvailable] == 'true'
+        if match.tuesdayStartTime == params[:tuesdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.tuesdayStartTime < params[:tuesdayTimes][0].to_i && match.tuesdayEndTime > params[:tuesdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.tuesdayStartTime > params[:tuesdayTimes][0].to_i && match.tuesdayStartTime < params[:tuesdayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+      if match.wednesdayStartTime != nil && params[:wednesdayAvailable] == 'true'
+        if match.wednesdayStartTime == params[:wednesdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.wednesdayStartTime < params[:wednesdayTimes][0].to_i && match.wednesdayEndTime > params[:wednesdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.wednesdayStartTime > params[:wednesdayTimes][0].to_i && match.wednesdayStartTime < params[:wednesdayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+      if match.thursdayStartTime != nil && params[:thursdayAvailable] == 'true'
+        if match.thursdayStartTime == params[:thursdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.thursdayStartTime < params[:thursdayTimes][0].to_i && match.thursdayEndTime > params[:thursdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.thursdayStartTime > params[:thursdayTimes][0].to_i && match.thursdayStartTime < params[:thursdayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+      if match.fridayStartTime != nil && params[:fridayAvailable] == 'true'
+        if match.fridayStartTime == params[:fridayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.fridayStartTime < params[:fridayTimes][0].to_i && match.fridayEndTime > params[:fridayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.fridayStartTime > params[:fridayTimes][0].to_i && match.fridayStartTime < params[:fridayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+      if match.saturdayStartTime != nil && params[:saturdayAvailable] == 'true'
+        if match.saturdayStartTime == params[:saturdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.saturdayStartTime < params[:saturdayTimes][0].to_i && match.saturdayEndTime > params[:saturdayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.saturdayStartTime > params[:saturdayTimes][0].to_i && match.saturdayStartTime < params[:saturdayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+      if match.sundayStartTime != nil && params[:sundayAvailable] == 'true'
+        if match.sundayStartTime == params[:sundayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.sundayStartTime < params[:sundayTimes][0].to_i && match.sundayEndTime > params[:sundayTimes][0].to_i
+          picked_match = match
+          break
+        end
+        if match.sundayStartTime > params[:sundayTimes][0].to_i && match.sundayStartTime < params[:sundayTimes][1].to_i
+          picked_match = match
+          break
+        end
+      end
+    end
+    
+    if picked_match == nil
+      create_new_match
+    else
+      # modify existing picked_match
+      picked_match.status = 'complete'
+      picked_match.user2Id = current_user.id
+      
+      # update similar foods
+      MatchToFood.where(matchId: picked_match.id).find_each do |food|
+        food.destroy unless params[:cuisines].include? food.foodId.to_s
+      end
+      
+      # update common times
+      if picked_match.mondayStartTime != nil && params[:mondayAvailable] == 'true'
+        if picked_match.mondayStartTime == params[:mondayTimes][0].to_i
+        end
+        if picked_match.mondayStartTime < params[:mondayTimes][0].to_i && picked_match.mondayEndTime > params[:mondayTimes][0].to_i
+          picked_match.mondayStartTime = params[:mondayTimes][0].to_i
+        end
+        picked_match.mondayEndTime = params[:mondayTimes][1].to_i if params[:mondayTimes][1].to_i < picked_match.mondayEndTime
+      else
+        picked_match.mondayStartTime = nil
+        picked_match.mondayEndTime = nil
+      end
+      if picked_match.tuesdayStartTime != nil && params[:tuesdayAvailable] == 'true'
+        if picked_match.tuesdayStartTime == params[:tuesdayTimes][0].to_i
+        end
+        if picked_match.tuesdayStartTime < params[:tuesdayTimes][0].to_i && picked_match.tuesdayEndTime > params[:tuesdayTimes][0].to_i
+          picked_match.tuesdayStartTime = params[:tuesdayTimes][0].to_i
+        end
+        picked_match.tuesdayEndTime = params[:tuesdayTimes][1].to_i if params[:tuesdayTimes][1].to_i < picked_match.tuesdayEndTime
+      else
+        picked_match.tuesdayStartTime = nil
+        picked_match.tuesdayEndTime = nil
+      end
+      if picked_match.wednesdayStartTime != nil && params[:wednesdayAvailable] == 'true'
+        if picked_match.wednesdayStartTime == params[:wednesdayTimes][0].to_i
+        end
+        if picked_match.wednesdayStartTime < params[:wednesdayTimes][0].to_i && picked_match.wednesdayEndTime > params[:wednesdayTimes][0].to_i
+          picked_match.wednesdayStartTime = params[:wednesdayTimes][0].to_i
+        end
+        picked_match.wednesdayEndTime = params[:wednesdayTimes][1].to_i if params[:wednesdayTimes][1].to_i < picked_match.wednesdayEndTime
+      else
+        picked_match.wednesdayStartTime = nil
+        picked_match.wednesdayEndTime = nil
+      end
+      if picked_match.thursdayStartTime != nil && params[:thursdayAvailable] == 'true'
+        if picked_match.thursdayStartTime == params[:thursdayTimes][0].to_i
+        end
+        if picked_match.thursdayStartTime < params[:thursdayTimes][0].to_i && picked_match.thursdayEndTime > params[:thursdayTimes][0].to_i
+          picked_match.thursdayStartTime = params[:thursdayTimes][0].to_i
+        end
+        picked_match.thursdayEndTime = params[:thursdayTimes][1].to_i if params[:thursdayTimes][1].to_i < picked_match.thursdayEndTime
+      else
+        picked_match.thursdayStartTime = nil
+        picked_match.thursdayEndTime = nil
+      end
+      if picked_match.fridayStartTime != nil && params[:fridayAvailable] == 'true'
+        if picked_match.fridayStartTime == params[:fridayTimes][0].to_i
+        end
+        if picked_match.fridayStartTime < params[:fridayTimes][0].to_i && picked_match.fridayEndTime > params[:fridayTimes][0].to_i
+          picked_match.fridayStartTime = params[:fridayTimes][0].to_i
+        end
+        picked_match.fridayEndTime = params[:fridayTimes][1].to_i if params[:fridayTimes][1].to_i < picked_match.fridayEndTime
+      else
+        picked_match.fridayStartTime = nil
+        picked_match.fridayEndTime = nil
+      end
+      if picked_match.saturdayStartTime != nil && params[:saturdayAvailable] == 'true'
+        if picked_match.saturdayStartTime == params[:saturdayTimes][0].to_i
+        end
+        if picked_match.saturdayStartTime < params[:saturdayTimes][0].to_i && picked_match.saturdayEndTime > params[:saturdayTimes][0].to_i
+          picked_match.saturdayStartTime = params[:saturdayTimes][0].to_i
+        end
+        picked_match.saturdayEndTime = params[:saturdayTimes][1].to_i if params[:saturdayTimes][1].to_i < picked_match.saturdayEndTime
+      else
+        picked_match.saturdayStartTime = nil
+        picked_match.saturdayEndTime = nil
+      end
+      if picked_match.sundayStartTime != nil && params[:sundayAvailable] == 'true'
+        if picked_match.sundayStartTime == params[:sundayTimes][0].to_i
+        end
+        if picked_match.sundayStartTime < params[:sundayTimes][0].to_i && picked_match.sundayEndTime > params[:sundayTimes][0].to_i
+          picked_match.sundayStartTime = params[:sundayTimes][0].to_i
+        end
+        picked_match.sundayEndTime = params[:sundayTimes][1].to_i if params[:sundayTimes][1].to_i < picked_match.sundayEndTime
+      else
+        picked_match.sundayStartTime = nil
+        picked_match.sundayEndTime = nil
+      end
+
+      picked_match.save
     end
   end
+  
+  private
+    
+    def create_new_match
+      # if no existing matches, create new one
+      new_match = Match.new
+      new_match.user1Id = current_user.id
+      
+      if Time.now.wday == 0
+        new_match.status = 'next week'
+      else
+        new_match.status = 'pending'
+      end
+    
+      if params[:mondayAvailable] == 'true'
+        new_match.mondayStartTime = params[:mondayTimes][0]
+        new_match.mondayEndTime = params[:mondayTimes][1]
+      end
+      if params[:tuesdayAvailable] == 'true'
+        new_match.tuesdayStartTime = params[:tuesdayTimes][0]
+        new_match.tuesdayEndTime = params[:tuesdayTimes][1]
+      end
+      if params[:wednesdayAvailable] == 'true'
+        new_match.wednesdayStartTime = params[:wednesdayTimes][0]
+        new_match.wednesdayEndTime = params[:wednesdayTimes][1]
+      end
+      if params[:thursdayAvailable] == 'true'
+        new_match.thursdayStartTime = params[:thursdayTimes][0]
+        new_match.thursdayEndTime = params[:thursdayTimes][1]
+      end
+      if params[:fridayAvailable] == 'true'
+        new_match.fridayStartTime = params[:fridayTimes][0]
+        new_match.fridayEndTime = params[:fridayTimes][1]
+      end
+      if params[:saturdayAvailable] == 'true'
+        new_match.saturdayStartTime = params[:saturdayTimes][0]
+        new_match.saturdayEndTime = params[:saturdayTimes][1]
+      end
+      if params[:sundayAvailable] == 'true'
+        new_match.sundayStartTime = params[:sundayTimes][0]
+        new_match.sundayEndTime = params[:sundayTimes][1]
+      end
+      new_match.save!
+      
+      params[:cuisines].each do |cuisine|
+        new_match_to_food = MatchToFood.new(matchId: new_match.id, foodId: cuisine)
+        new_match_to_food.save
+      end
+    end
+    
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,15 @@
 class Match < ApplicationRecord
+  scope :pending, -> { where(status: 'pending') }
+  scope :pending_or_complete, -> { where("matches.status = 'pending' OR matches.status = 'complete'") }
+  scope :with_user1, ->(user_id) { joins("INNER JOIN users ON users.id = matches.user1Id AND users.id = ", user_id).distinct }
+  scope :with_user2, ->(user_id) { joins("INNER JOIN users ON users.id = matches.user2Id AND users.id = ", user_id).distinct }
+  scope :with_school, ->(school) { joins("INNER JOIN users ON users.id = matches.user1Id AND users.school = '" + school + "'").distinct }
+  
+  def self.with_foods(foods)
+    food_filter = "match_to_foods.foodId = " + foods[0].to_s
+    foods[1..-1].each do |food|
+      food_filter << " OR match_to_foods.foodId = " + food.to_s
+    end
+    joins("INNER JOIN match_to_foods ON match_to_foods.matchId = matches.id AND (" + food_filter + ")")
+  end
 end
