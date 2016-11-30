1fcde0ea0e481868a8f9259ef30f8a7894034db2
Jonathan Cheung <jcheung142@gmail.com>
2016-11-29 23:36:05 -0800

    reverted devise authentication and cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,9 @@
 #used for debugging and outputting error
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-=begin
+
 every :monday, :at => '1am' do
   rake "clear_matches"
 end
-=end
 
 =begin
 to run on development:
@@ -12,7 +11,7 @@ to run on development:
 2) crontab -l //verifies whenever ran cron task
 
 TEST CASE:
-=end
 every 1.minutes do
 	rake "clear_matches"
 end
+=end
