545761807e29b93f56c5f9b4c5655166b1eab00d
Abhijoy Saha <abhijoysaha25@gmail.com>
2016-12-01 01:16:42 +0000

    Added server-side caching for math_not_found

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,4 +2,7 @@ module MatchesHelper
   def cache_key_for_match(match)
     "match-#{match.id}-#{match.updated_at}-#{match.created_at}"
   end
+  def cache_key_for_match_not_found(match)
+    "match_not_found-#{match.id}-#{match.updated_at}-#{match.created_at}"
+  end
 end
