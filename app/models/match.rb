e7570e3b8623561f202974b97596e4b82d8d2851
Aaron Cheng <ac4466@gmail.com>
2016-12-01 01:32:56 +0000

    Fix missing end in match.rb

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,13 +5,14 @@ class Match < ApplicationRecord
   scope :with_user1, ->(user_id) { joins("INNER JOIN users ON users.id = matches.user1Id AND users.id = ", user_id).distinct }
   scope :with_user2, ->(user_id) { joins("INNER JOIN users ON users.id = matches.user2Id AND users.id = ", user_id).distinct }
   scope :with_school, ->(school) { joins("INNER JOIN users ON users.id = matches.user1Id AND users.school = '" + school + "'").distinct }
-  
+
   def self.with_foods(foods)
     logger.debug "foods is : #{foods}"
     food_filter = "match_to_foods.foodId = " + foods[0].to_s
     foods[1..-1].each do |food|
-    food_filter << " OR match_to_foods.foodId = " + food.to_s
+      food_filter << " OR match_to_foods.foodId = " + food.to_s
     end
     logger.debug "food_filter is: #{food_filter}"
     joins("INNER JOIN match_to_foods ON match_to_foods.matchId = matches.id AND (" + food_filter + ")")
+  end
 end
