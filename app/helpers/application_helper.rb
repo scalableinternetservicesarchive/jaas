038884874a2bfdff0b676f729e64e03313ac4f00
Abhijoy Saha <abhijoysaha25@gmail.com>
2016-12-01 00:39:49 +0000

    Added server-side caching for math_found

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def cache_key_for_stock(stock)
-    "stock-#{stock.updated_at}"
-  end
-  def cache_key_for_stock_list
-    "stock-list-#{Stock.maximum(:updated_at)}-#{Stock.maximum(:created_at)}"
-  end
 end
--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,5 @@
 module MatchesHelper
+  def cache_key_for_match(match)
+    "match-#{match.id}-#{match.updated_at}-#{match.created_at}"
+  end
 end
